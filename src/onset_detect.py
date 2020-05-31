be913c60b989afc70513e12abac24a7366fcaf14
Brennan Coslett <brennan@coslett.org>
2020-05-31 22:47:37 +0200

    functioning onset

--- src/onset_detect.py
+++ src/onset_detect.py
@@ -3,17 +3,15 @@ from framework import *
 def onsetDetector(input_dir):
     inputted_files = import_dir(input_dir)
     frame_length = 0
-    for i,file in enumerate(inputted_files):
+    for file in inputted_files:
         frame_energies, frame_length = calcFrameEnergies(file)
+        #print(str(frame_energies[0:100].std()**.5) + " " + str(frame_energies[0:150].std()**.5))
         file_pp_min =  frame_energies[:100].std()
-        # plot frame energies over time
-        # plt.plot(frame_energies)
-        # plt.title(f'{file.name} Frames: {frame_energies.size}')
-        # plt.show()
         # calculate peaks
+        batch_size = 5
         filePeaks = []
-        for value in range(5, frame_energies.size,5):
-            localMax = np.argmax(frame_energies[value - 5:value])
+        for value in range(batch_size, frame_energies.size,batch_size):
+            localMax = np.argmax(frame_energies[value - batch_size:value])
             try:
                 if (frame_energies[localMax + value] > file_pp_min):
                     if np.isin(localMax+ value, filePeaks, invert=True):    
@@ -22,7 +20,7 @@ def onsetDetector(input_dir):
                 pass
             
         #log to file
-        logfile = Path.joinpath(Path(input_dir),'pr',file.stem + ".onsets.pr")
+        logfile = Path.joinpath(Path(input_dir),file.stem + ".onsets.pr")
         with logfile.open("w+") as f:
             for peak in filePeaks:
                 f.write(f'{(peak * frame_length):0.9f}\n')
